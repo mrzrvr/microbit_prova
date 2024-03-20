04eddcf3bbe0997ed82ad4353be6031ba4e1b59a
mrzrvr <103333550+mrzrvr@users.noreply.github.com>
2024-03-20 11:08:00 +0100

    Aggiorna pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
-music._play_default_background(music.built_in_playable_melody(Melodies.PRELUDE),
+basic.show_leds("""
+    # . . . .
+    . # . . .
+    . . # . .
+    . . . # .
+    . . . . #
+    """)
+music._play_default_background(music.built_in_playable_melody(Melodies.DADADADUM),
     music.PlaybackMode.IN_BACKGROUND)
+basic.show_leds("""
+    . . . . #
+    . . . # .
+    . . # . .
+    . # . . .
+    # . . . .
+    """)
+basic.show_leds("""
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    """)
+basic.pause(3000)
 
 def on_forever():
     basic.show_icon(IconNames.SQUARE)
+    basic.show_icon(IconNames.SMALL_SQUARE)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        """)
     basic.show_icon(IconNames.SMALL_SQUARE)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,20 +1,27 @@
 basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
+    # . . . .
+    . # . . .
+    . . # . .
+    . . . # .
+    . . . . #
     `)
 music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
 basic.showLeds(`
+    . . . . #
+    . . . # .
+    . . # . .
+    . # . . .
+    # . . . .
+    `)
+basic.showLeds(`
     . . . . .
     . . . . .
-    . . . . .
+    . . # . .
     . . . . .
     . . . . .
     `)
 basic.pause(3000)
-basic.forever(function () {
+basic.forever(function on_forever() {
     basic.showIcon(IconNames.Square)
     basic.showIcon(IconNames.SmallSquare)
     basic.showLeds(`
