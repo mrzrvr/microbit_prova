6e0a3a973a38dbd24793f8d1fc9d5400746a13f6
mrzrvr <103333550+mrzrvr@users.noreply.github.com>
2024-03-20 11:02:11 +0100

    prova musica e led

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
 basic.forever(function () {
     basic.showIcon(IconNames.Square)
     basic.showIcon(IconNames.SmallSquare)
